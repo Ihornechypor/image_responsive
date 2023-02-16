2798e07a86c30094ce992a81599dc57e91e5d2d9
IhorNechypor <ihor.nechypor@capital.com>
2023-02-16 12:10:17 +0100

    updates

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -15,17 +15,43 @@ const delDist = (done) => {
 
 const convertToInt = (width, ratio) => Math.trunc(width * ratio) ? Math.trunc(width * ratio) : 1;
 
-const img = () => src(config.srcFiles)
-  .pipe(sharpResponsive({
-    formats: [ 
-      { width: (metadata) => convertToInt(metadata.width, 0.05), format: "webp", rename: { suffix: "-ph" }, webpOptions: config.webpOptions},
-      { width: (metadata) => metadata.width, format: "webp", rename: { suffix: "@2x" }, webpOptions: config.webpOptions},
-      { width: (metadata) => metadata.width, rename: { suffix: "@2x" }, pngOptions: config.pngOptions, jpegOptions: config.jpegOptions},
-      { width: (metadata) => convertToInt(metadata.width, 0.5), format: "webp",webpOptions: config.webpOptions},
-      { width: (metadata) => convertToInt(metadata.width, 0.5), pngOptions: config.pngOptions, jpegOptions: config.jpegOptions}
+const img = () => src(config.srcFiles).pipe(sharpResponsive({
+    formats: [
+        {
+            width: (metadata) => convertToInt(metadata.width, 0.05),
+            format: "webp",
+            rename: {
+                suffix: "-ph"
+            },
+            webpOptions: config.webpOptions
+        },
+        {
+            width: (metadata) => metadata.width,
+            format: "webp",
+            rename: {
+                suffix: "@2x"
+            },
+            webpOptions: config.webpOptions
+        },
+        {
+            width: (metadata) => metadata.width,
+            rename: {
+                suffix: "@2x"
+            },
+            pngOptions: config.pngOptions,
+            jpegOptions: config.jpegOptions
+        },
+        {
+            width: (metadata) => convertToInt(metadata.width, 0.5),
+            format: "webp",
+            webpOptions: config.webpOptions
+        }, {
+            width: (metadata) => convertToInt(metadata.width, 0.5),
+            pngOptions: config.pngOptions,
+            jpegOptions: config.jpegOptions
+        }
     ]
-  }))
-  .pipe(dest(config.dist)); 
+})).pipe(dest(config.dist));
 
 
 task('default', series(delDist, img));
